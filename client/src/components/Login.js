cea0182c2ca24bac1ef2a49a3e40b006a1046007
hsundhar4 <hsundhar4@gmail.com>
2025-08-17 13:36:56 +0530

    Add files via upload

---
+++ client/src/components/Login.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Login({ setToken, setUsername }) {
+  const [form, setForm] = useState({ email: '', password: '' });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await axios.post('http://localhost:5000/api/auth/login', form);
+    setToken(res.data.token);
+    setUsername(res.data.username);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input placeholder="Email" onChange={(e) => setForm({ ...form, email: e.target.value })} />
+      <input type="password" placeholder="Password" onChange={(e) => setForm({ ...form, password: e.target.value })} />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default Login;
