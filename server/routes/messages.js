e9aa711d129bb92a0dd200b432436c3eb6383126
hsundhar4 <hsundhar4@gmail.com>
2025-08-17 15:20:54 +0530

    Update messages.js

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { Message } = require('../models/Message');
+const Message = require('../models/Message');
 const router = express.Router();
 
 router.get('/:room', async (req, res) => {
