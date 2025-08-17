a0b0c17992716eb8a77a15308331a5b7552b9e64
hsundhar4 <hsundhar4@gmail.com>
2025-08-17 15:53:24 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,18 +8,23 @@ function App() {
   const [username, setUsername] = useState('');
   const [room, setRoom] = useState('');
 
-  if (!token) return <Login setToken={setToken} setUsername={setUsername} />;
-  if (!room) return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h2>Welcome, {username}</h2>
-      <input
-        placeholder="Enter Room Name"
-        value={room}
-        onChange={(e) => setRoom(e.target.value)}
-      />
-      <button onClick={() => setRoom(room)}>Join</button>
-    </div>
-  );
+  if (!token) {
+    return <Login setToken={setToken} setUsername={setUsername} />;
+  }
+
+  if (!room) {
+    return (
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <h2>Welcome, {username}</h2>
+        <input
+          placeholder="Enter Room Name"
+          value={room}
+          onChange={(e) => setRoom(e.target.value)}
+        />
+        <button onClick={() => setRoom(room)}>Join</button>
+      </div>
+    );
+  }
 
   return <Chat room={room} username={username} />;
 }
