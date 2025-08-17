7a24406057ce91be24de59c3ad664353a44d02a9
hsundhar4 <hsundhar4@gmail.com>
2025-08-17 14:00:40 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,24 @@
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const socketIO = require('socket.io');
+const path = require('path');
+const messageRoutes = require('./routes/messages');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server, {
+  cors: {
+    origin: 'http://localhost:3000', // Change this to your Render frontend URL after deployment
+    methods: ['GET', 'POST']
+  }
+});
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
 // Routes
 app.use('/api/messages', messageRoutes);
 
@@ -8,7 +29,7 @@ mongoose.connect('mongodb://localhost:27017/chatapp', {
 }).then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB error:', err));
 
-// Serve React frontend
+// Serve React frontend build
 app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get("*", (req, res) => {
@@ -20,7 +41,7 @@ io.on('connection', (socket) => {
   console.log('New client connected:', socket.id);
 
   socket.on('sendMessage', (data) => {
-    io.emit('receiveMessage', data);
+    io.emit('receiveMessage', data); // Broadcast to all clients
   });
 
   socket.on('disconnect', () => {
